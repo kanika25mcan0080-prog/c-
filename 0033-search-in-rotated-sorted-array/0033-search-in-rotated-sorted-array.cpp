c1041d26042bb83f9dc7bd12aaf701228ced4c3f
kanika25mcan0080-prog <kanika.25mcan0080@jecrcu.edu.in>
2026-06-23 12:47:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (96.17%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,56 @@
+class Solution 
+{
+    public:
+    int search(vector<int>& nums, int target) 
+    {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            }//end of if block
+            //check if the left half is sorted
+            if(nums[left]<=nums[mid])
+            {
+                if(target>=nums[left] && target<nums[mid])
+                {
+                   right=mid-1; 
+                }
+                else
+                {
+                    left=mid+1;
+                }
+            
+            }// end of if block
+            else
+            {
+                if(target>nums[mid] && target<=nums[right])
+                {
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
+            }// end of else block
+        }
+        return -1;
+    }//end of search()
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+

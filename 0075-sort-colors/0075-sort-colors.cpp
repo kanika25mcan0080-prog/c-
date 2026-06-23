52f4255c074c74f23a32770db81af62753153b2a
kanika25mcan0080-prog <kanika.25mcan0080@jecrcu.edu.in>
2026-06-23 12:50:42 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.31%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public:
+    void sortColors(vector<int>& nums) //Dutch flag algo
+    {
+        int left =0;
+        int temp=0;
+        int right=nums.size()-1;
+        while(temp<=right)
+        {
+            if(nums[temp]==0)
+            {
+                swap(nums[temp],nums[left]);
+                left++;
+                temp++;
+            }//end of if block
+            else if(nums[temp]==2)
+            {
+                swap(nums[temp],nums[right]);
+                right--;
+                
+            }//end of else if block
+            else
+            {
+                temp++;
+            }//end of else block
+        }//end of while loop
+    }//end of sortColors()
+};
